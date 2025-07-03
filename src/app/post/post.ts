917ed4b62966d8b460cac000716ad8416d2a00eb
Daniel Kandalaft <kandddany1985@gmail.com>
2025-07-03 10:59:03 +0300

    Added Exporting More Lifecycle Hooks Example

--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,4 +1,17 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -6,7 +19,17 @@ import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
-export class Post implements OnInit {
+export class Post
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input('img') postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
 
@@ -16,5 +39,33 @@ export class Post implements OnInit {
 
   ngOnInit() {
     console.log('ngOnInit() called', this.postImg);
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges() called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoChcek() called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked() called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit() called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked() called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit() called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy() called');
   }
 }
