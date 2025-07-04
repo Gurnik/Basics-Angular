34d8c9169cc83a3a0ce15d9dabdfa7e7d749c501
Daniel Kandalaft <kandddany1985@gmail.com>
2025-07-04 12:47:44 +0300

    Added Pipe Basics Example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Post } from './post/post';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Post],
+  imports: [RouterOutlet, Post, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'basics';
 
-  protected name = 'Luis';
+  protected name = 'daniel kandalaft';
   protected imgURL = 'https://picsum.photos/id/237/500/500';
 
   getName() {
